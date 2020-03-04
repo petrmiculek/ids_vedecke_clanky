2ba6f9f8a724988a267dccf8e9bfd3b22d494b93
Petr Miculek <petr.miculek@gmail.com>
2020-03-04 14:01:23 +0100

    Drop table for all tables, person finished

--- script.sql
+++ script.sql
@@ -1,11 +1,21 @@
-DROP TABLE Person CASCADE CONSTRAINTS;
-DROP TABLE Institution CASCADE CONSTRAINTS;
+DROP TABLE Person CASCADE CONSTRAINTS; -- osoba
+DROP TABLE Institution CASCADE CONSTRAINTS; -- instituce
+DROP TABLE Publisher; -- vydavatel
+DROP TABLE Article; -- clanek
+DROP TABLE Magazine; -- casopis
+DROP TABLE MagazineIssue; -- vydani casopisu
+DROP TABLE Share; -- podil
+DROP TABLE Contribution; -- prispevek
+DROP TABLE ImpactFactorHistory; -- historie impakt faktoru
 
 CREATE TABLE Person(
-    ID INTEGER,
-
-    PRIMARY KEY (ID)
-);
+    id INT DEFAULT Person_seq.NEXTVAL NOT NULL PRIMARY KEY,
+    name_first VARCHAR(31),
+    name_last VARCHAR(31),
+    email VARCHAR(63),
+    research_topic VARCHAR(1023),
+    research_keywords VARCHAR(255)
+    );
 
 CREATE TABLE Institution (
   id INT NOT NULL PRIMARY KEY,
