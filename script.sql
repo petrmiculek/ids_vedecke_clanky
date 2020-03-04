fd6edce68682b24e3cdf4cd443019595c19bf85a
Petr Miculek <petr.miculek@gmail.com>
2020-03-04 15:21:20 +0100

    ArticleShare, Magazine, MagazineIssue, Publisher, TODO notes

--- script.sql
+++ script.sql
@@ -1,6 +1,5 @@
 DROP TABLE Person CASCADE CONSTRAINTS; -- osoba
 DROP TABLE Institution CASCADE CONSTRAINTS; -- instituce
-DROP SEQUENCE Person_seq;
 DROP TABLE Publisher; -- vydavatel
 DROP TABLE Article; -- clanek
 DROP TABLE Contribution; -- prispevek
@@ -9,7 +8,9 @@ DROP TABLE Magazine; -- casopis
 DROP TABLE MagazineIssue; -- vydani casopisu
 DROP TABLE ArticleShare; -- podil na clanku
 DROP TABLE ImpactFactorHistory; -- historie impakt faktoru (casopisu)
+DROP TABLE Citation; -- citace (clanku)
 
+DROP SEQUENCE Person_seq;
 
 CREATE SEQUENCE Person_seq
     START WITH 100000
@@ -40,6 +41,19 @@ CREATE TABLE Contribution(
 
 );
 
+CREATE TABLE Citation(
+    id INT NOT NULL PRIMARY KEY,
+    id_citing INT NOT NULL,
+    id_cited INT NOT NULL,
+
+    CONSTRAINT Citation_Article_citing_fk
+        FOREIGN KEY (id_citing) REFERENCES Article (id) ON DELETE CASCADE,
+
+    CONSTRAINT Citation_Article_cited_fk
+        FOREIGN KEY (id_citing) REFERENCES Article (id) ON DELETE CASCADE
+
+);
+
 CREATE TABLE TechnicalReport(
     id INT NOT NULL PRIMARY KEY,
     institution_id INT NOT NULL,
@@ -53,20 +67,37 @@ CREATE TABLE TechnicalReport(
 
 
 CREATE TABLE ArticleShare(
-    percentage NUMBER CHECK(percentage > 0 and percentage <= 100) -- non-zero
-    -- poradi autoru?
+    author_id INT NOT NULL,
+    article_id INT NOT NULL,
+    author_order INT NOT NULL, -- poradi autora, najit vhodnejsi oznaceni
+    percentage NUMBER CHECK(percentage > 0 and percentage <= 100), -- non-zero
+    -- ma smysl na urovni SQL hlidat Suma(percentage) == 100?
+
+    CONSTRAINT ArticleShare_Person_fk
+        FOREIGN KEY (author_id) REFERENCES Person (id) ON DELETE CASCADE,
+        -- on delete nemusi byt cascade?
+
+    CONSTRAINT ArticleShare_Article_fk
+        FOREIGN KEY (article_id) REFERENCES Article (id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE Magazine(
     id INT NOT NULL PRIMARY KEY,
-    name VARCHAR(63)
-    -- impact factor history (separate table)
+    name VARCHAR(63),
+    publisher_id INT NOT NULL,
+    CONSTRAINT ImpactFactorHistory_Magazine_fk
+        FOREIGN KEY (publisher_id) REFERENCES Publisher (id) ON DELETE CASCADE
+
+    -- (Magazine is referenced by impact factor history)
 );
 
 
 CREATE TABLE MagazineIssue(
-
+    id INT NOT NULL PRIMARY KEY,
+    date_published DATE,
+    impact_factor_value NUMBER -- decimal value?
+    -- also, does the attribute make sense together with table ImpactFactorHistory?
 );
 
 
@@ -78,7 +109,8 @@ CREATE TABLE Article(
 );
 
 CREATE TABLE Publisher(
-    name_company VARCHAR(31) NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY,
+    name_company VARCHAR(31) NOT NULL,
     name_owner VARCHAR(63)
 
 );
@@ -102,3 +134,8 @@ FROM Person;
 
 SELECT *
 FROM Institution;
+
+-- TODO
+-- NOT NULL constraints - check existing, add further?
+-- SEQUENCE for primary keys generation
+-- ON DELETE .___. - check existing, discuss
