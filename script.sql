c469a2c38be947db4a281da0a76b2378ddd60583
Petr Miculek <petr.miculek@gmail.com>
2020-03-08 15:55:35 +0100

    code formatting

--- script.sql
+++ script.sql
@@ -38,78 +38,86 @@ CREATE SEQUENCE Publisher_seq
     INCREMENT BY 1;
 
 
-CREATE TABLE Institution (
+CREATE TABLE Institution
+(
     code INT DEFAULT Institution_seq.NEXTVAL PRIMARY KEY,
     name VARCHAR(63)
 );
 
-CREATE TABLE Person(
-    id INT DEFAULT Person_seq.NEXTVAL PRIMARY KEY,
-    name_first VARCHAR(31),
-    name_last VARCHAR(31),
-    email VARCHAR(63),
-    research_topic VARCHAR(1023),
+CREATE TABLE Person
+(
+    id                INT DEFAULT Person_seq.NEXTVAL PRIMARY KEY,
+    name_first        VARCHAR(31),
+    name_last         VARCHAR(31),
+    email             VARCHAR(63),
+    research_topic    VARCHAR(1023),
     research_keywords VARCHAR(255),
 
-    institution_id INT NOT NULL,
+    institution_id    INT NOT NULL,
 
     CONSTRAINT Institution_Person_fk
         FOREIGN KEY (institution_id) REFERENCES Institution (code) ON DELETE CASCADE
 );
 
-CREATE TABLE Article(
-    id INT DEFAULT Article_seq.NEXTVAL PRIMARY KEY, -- document object identifier
+CREATE TABLE Article
+(
+    id           INT DEFAULT Article_seq.NEXTVAL PRIMARY KEY, -- document object identifier
     article_name VARCHAR(63)
     --- missing author???
 );
 
-CREATE TABLE ArticleShare(
-    author_id INT NOT NULL,
-    article_id INT NOT NULL,
+CREATE TABLE ArticleShare
+(
+    author_id    INT NOT NULL,
+    article_id   INT NOT NULL,
     author_order INT NOT NULL,
-    percentage NUMBER CHECK(percentage > 0 and percentage <= 100), -- non-zero
+    percentage   NUMBER CHECK (percentage > 0 and percentage <= 100), -- non-zero
 
     CONSTRAINT ArticleShare_Person_fk
         FOREIGN KEY (author_id) REFERENCES Person (id) ON DELETE CASCADE,
-        -- on delete nemusi byt cascade?
+    -- on delete nemusi byt cascade?
 
     CONSTRAINT ArticleShare_Article_fk
         FOREIGN KEY (article_id) REFERENCES Article (id) ON DELETE CASCADE
 );
 
-CREATE TABLE Publisher(
-    id INT DEFAULT Publisher_seq.NEXTVAL PRIMARY KEY,
+CREATE TABLE Publisher
+(
+    id           INT DEFAULT Publisher_seq.NEXTVAL PRIMARY KEY,
     name_company VARCHAR(31) UNIQUE NOT NULL, -- vydavatel je identifikovan jmenem
-    name_owner VARCHAR(63)
+    name_owner   VARCHAR(63)
 
 );
 
-CREATE TABLE Magazine(
-    id INT DEFAULT Magazine_seq.NEXTVAL PRIMARY KEY,
-    name VARCHAR(63),           -- name and publisher_id MUST BE UNIQUE together
+CREATE TABLE Magazine
+(
+    id           INT DEFAULT Magazine_seq.NEXTVAL PRIMARY KEY,
+    name         VARCHAR(63), -- name and publisher_id MUST BE UNIQUE together
     publisher_id INT NOT NULL,
 
     CONSTRAINT Magazine_Publisher_fk
         FOREIGN KEY (publisher_id) REFERENCES Publisher (id) ON DELETE CASCADE,
 
-    CONSTRAINT name_publisher_unique UNIQUE(publisher_id, name)
+    CONSTRAINT name_publisher_unique UNIQUE (publisher_id, name)
 );
 
-CREATE TABLE ImpactFactorHistory(
+CREATE TABLE ImpactFactorHistory
+(
     magazine_id INT NOT NULL,
-    value NUMBER,
-    year INT CHECK(year >= 1900),
+    value       NUMBER,
+    year        INT CHECK (year >= 1900),
     -- foreign key -> magazine
     CONSTRAINT ImpactFactorHistory_Magazine_fk
         FOREIGN KEY (magazine_id) REFERENCES Magazine (id) ON DELETE CASCADE
 
 );
 
-CREATE TABLE TechnicalReport(
-    id INT PRIMARY KEY, -- primary key referencing
+CREATE TABLE TechnicalReport
+(
+    id             INT PRIMARY KEY, -- primary key referencing
     institution_id INT NOT NULL,
 
-    CONSTRAINT TechRep_Article_fk -- generalization relationship
+    CONSTRAINT TechRep_Article_fk   -- generalization relationship
         FOREIGN KEY (id) REFERENCES Article (id) ON DELETE CASCADE,
 
     CONSTRAINT TechRep_Institution_fk
@@ -130,21 +138,22 @@ CREATE TABLE MagazineIssue
         FOREIGN KEY (magazine_id) REFERENCES Magazine (id) ON DELETE CASCADE
 );
 
-CREATE TABLE Contribution(
-                             id                    INT PRIMARY KEY,
+CREATE TABLE Contribution
+(
+    id                    INT PRIMARY KEY,
 
-                             magazine_id           INT NOT NULL,
-                             magazine_issue_year   INT NOT NULL,
-                             magazine_issue_number INT NOT NULL,
+    magazine_id           INT NOT NULL,
+    magazine_issue_year   INT NOT NULL,
+    magazine_issue_number INT NOT NULL,
 
-                             other_citations_count INT NOT NULL, -- how many times is this article cited by articles
+    other_citations_count INT NOT NULL, -- how many times is this article cited by articles
     -- which are not present in this database
 
-                             CONSTRAINT Contribution_Article_fk  -- generalization relationship
-                                 FOREIGN KEY (magazine_id) REFERENCES Article (id) ON DELETE CASCADE,
+    CONSTRAINT Contribution_Article_fk  -- generalization relationship
+        FOREIGN KEY (magazine_id) REFERENCES Article (id) ON DELETE CASCADE,
 
-                             CONSTRAINT Contribution_MagazineIssue_fk
-                                 FOREIGN KEY (magazine_id, magazine_issue_year, magazine_issue_number) REFERENCES MagazineIssue (year, issue_number) ON DELETE CASCADE
+    CONSTRAINT Contribution_MagazineIssue_fk
+        FOREIGN KEY (magazine_id, magazine_issue_year, magazine_issue_number) REFERENCES MagazineIssue (year, issue_number) ON DELETE CASCADE
 );
 
 INSERT INTO Institution
@@ -177,17 +186,17 @@ VALUES (DEFAULT, 'Patrick', 'Chimney', 'pchimney@google.com', 'Measurement of ab
 INSERT INTO Article
 VALUES (567, 'Locus classification in chromosome');
 INSERT INTO Article
-    VALUES(672, 'Blah-blah article');
+VALUES (672, 'Blah-blah article');
 
 INSERT INTO ArticleShare
-    VALUES(100000, 567, DEFAULT, 40);
+VALUES (100000, 567, DEFAULT, 40);
 INSERT INTO ArticleShare
-    VALUES(100001, 567, DEFAULT, 60);
+VALUES (100001, 567, DEFAULT, 60);
 INSERT INTO ArticleShare
-    VALUES(100001, 672, DEFAULT, 100);
+VALUES (100001, 672, DEFAULT, 100);
 
 INSERT INTO TechnicalReport
-    VALUES(567, 105);
+VALUES (567, 105);
 
 INSERT INTO Publisher
 VALUES (300, 'Best Publishers Ever (BPE)', 'Mark Fitch');
